06e9bec73844ccb383cf527f5ece1c950632d3a3
ahnytae <h.t.ahn93@gmail.com>
2021-10-31 00:02:24 +0900

    feature: make a getUserMedia initail class

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,1 +1,5 @@
-console.log('FE');
+import DeviceController from './deviceController/DeviceController';
+import GetUserMedia from './deviceController/getUserMedia';
+import GetUserMediaError from './deviceController/GetUserMediaError';
+
+export { DeviceController, GetUserMedia, GetUserMediaError };
